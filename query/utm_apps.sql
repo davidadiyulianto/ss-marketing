38dd13ffc4a663e7d749fbd6c9e8ae1b67691b1e
williamwil777 <william.wil.777@gmail.com>
2017-02-17 17:53:57 +0700

    Update utm_apps.sql

--- query/utm_apps.sql
+++ query/utm_apps.sql
@@ -1,1 +1,48 @@
+CASE 
+	WHEN media_source = 'Facebook Ads' 
+		AND
+		agency ILIKE '%FB%'
+		THEN
+		'Facebook Ads'
 
+	WHEN media_source = 'Facebook Ads' 
+		AND
+		agency NOT ILIKE '%FB%'
+		THEN
+		agency
+
+	WHEN agency = 'rapido' 
+		THEN 
+		'rapido'
+
+	WHEN agency = 'vizury' 
+		THEN 
+		'vizury'
+	
+	WHEN agency = 'socialclicks' 
+		THEN 
+		'socialclicks'
+
+	WHEN agency = 'vserv'
+		THEN
+		'vserv'
+
+    WHEN agency = 'artofclick' 
+    	THEN 
+    	'artofclick'
+                 
+    WHEN agency = 'ibd' 
+    	THEN 
+    	'cheetahmobile'
+                    
+    WHEN agency = 'revx' 
+    	THEN 
+    	'revx'
+                    
+    WHEN attribution_type = 'organic' 
+    	THEN 'Organic'
+	
+	ELSE
+		media_source
+
+	END AS channel
