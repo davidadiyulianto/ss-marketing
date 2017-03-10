252050152f7229c0639b43cddffeb3bb5ddb1341
williamwil777 <william.wil.777@gmail.com>
2017-03-10 17:16:29 +0700

    revisi

--- query/utm_apps.sql
+++ query/utm_apps.sql
@@ -1,48 +1,191 @@
 CASE 
-	WHEN media_source = 'Facebook Ads' 
-		AND
-		agency ILIKE '%FB%'
-		THEN
-		'Facebook Ads'
+    WHEN utm_source = 'inmobi_int'
+        AND 
+        utm_campaign = 'NewCustomer'
+    THEN 'TRT-inmobi'
 
-	WHEN media_source = 'Facebook Ads' 
-		AND
-		agency NOT ILIKE '%FB%'
-		THEN
-		agency
+    WHEN utm_source = 'inmobi_int'
+        AND 
+        (
+        utm_campaign = 'MOFU-Campaign'
+        OR
+        utm_campaign = 'Dormant'
+        )
+    THEN 'MOFU-inmobi'
 
-	WHEN agency = 'rapido' 
-		THEN 
-		'rapido'
+    WHEN utm_source = 'criteo_int'
+         AND 
+         utm_campaign = 'ExistingCustomer'
+    THEN 'MOFU-CRITEO'
 
-	WHEN agency = 'vizury' 
-		THEN 
-		'vizury'
-	
-	WHEN agency = 'socialclicks' 
-		THEN 
-		'socialclicks'
+    WHEN utm_source = 'vizury_int'
+         AND
+         utm_campaign = 'NewCustomer'
+    THEN 'TRT-vizury'
 
-	WHEN agency = 'vserv'
-		THEN
-		'vserv'
+    WHEN utm_source = 'criteo_int'
+         AND 
+         utm_campaign = 'NewCustomer'
+    THEN 'TRT-CRITEO'
 
-    WHEN agency = 'artofclick' 
-    	THEN 
-    	'artofclick'
-                 
-    WHEN agency = 'ibd' 
-    	THEN 
-    	'cheetahmobile'
-                    
-    WHEN agency = 'revx' 
-    	THEN 
-    	'revx'
-                    
-    WHEN attribution_type = 'organic' 
-    	THEN 'Organic'
-	
-	ELSE
-		media_source
+    WHEN utm_source ILIKE 'OneSignal'
+         OR
+         utm_source ILIKE 'Parse'
+    THEN 'Pushnot'
 
-	END AS channel
+    WHEN utm_source ILIKE '%FACEBOOK%' 
+         AND 
+         (
+         utm_campaign ILIKE '%-RT-CU-%' 
+         OR
+         utm_campaign ILIKE '%CUSTOMER%'
+         )
+     THEN 'MOFU-FB'
+
+    WHEN utm_source = 'revx_int'
+    THEN 'MOFU-revx'
+
+    WHEN utm_source ILIKE 'CRITEO'
+        AND
+        (
+        utm_medium ILIKE '%FV'
+        OR
+        utm_campaign ILIKE '%FV-%'
+        OR
+        utm_campaign = 'beli2gratis1'
+        OR
+        utm_campaign ILIKE 'coupon%%'
+        OR
+        utm_campaign = 'visitor-tvc-december'
+        )
+    THEN 'TRT-CRITEO'
+
+    WHEN utm_source ILIKE 'CRITEO'
+        AND
+        (
+        utm_medium ILIKE '%FC'
+        OR
+        utm_campaign ILIKE '%FC-%'
+        )
+    THEN 'MOFU-CRITEO'
+
+    WHEN utm_medium = 'messages'
+    THEN 'chat'
+
+    WHEN utm_source = 'googleadwordsoptimizer_int'
+    THEN 'TRT-google'
+
+    WHEN utm_source = 'Markotop'
+    THEN 'Pushnot'
+
+    WHEN utm_source ILIKE 'FACEBOOK' 
+        AND
+        utm_campaign ILIKE '%-RT-%' 
+        AND
+        utm_campaign NOT ILIKE '%-CU-%' 
+        AND
+        utm_campaign NOT ILIKE '%CUSTOMER%'
+    THEN 'TRT-FB'
+
+    WHEN utm_source ILIKE 'FACEBOOK'
+        AND 
+        utm_campaign ILIKE 'FB-PE'
+    THEN 'FB-PE'
+
+    WHEN utm_source = 'line'
+        AND
+        utm_medium = 'chat'
+    THEN 'LINE-chat'
+
+    WHEN utm_source = 'line'
+        AND
+        utm_medium = 'social'
+    THEN 'SC-LINE'
+
+    WHEN utm_source = 'bbm'
+        AND
+        utm_medium = 'social'
+    THEN 'SC-BBM'
+
+    WHEN utm_source = 'google'
+         AND
+         utm_campaign IS NOT NULL
+    THEN 'TRT-google'
+
+    WHEN utm_source = 'google'
+         AND
+         utm_campaign IS NULL
+    THEN 'Organic-google'
+
+    WHEN utm_source = 'quiz-maker'
+    THEN 'BNB'
+
+    WHEN utm_medium ILIKE 'email'
+    THEN 'email'
+
+    WHEN utm_source ILIKE 'RTBHOUSE'
+        AND
+        (
+        utm_medium ILIKE 'CPC' 
+        OR 
+        utm_medium ILIKE 'PAID-DISPLAY'
+        )
+        AND
+        utm_campaign not ILIKE '%buyer%'
+    THEN 'TRT-RTBHOUSE'
+
+    WHEN utm_source ILIKE 'RTBHOUSE'
+            AND
+            (
+            utm_medium ILIKE 'CPC' 
+            or 
+            utm_medium ILIKE 'PAID-DISPLAY'
+            )
+            AND
+            utm_campaign ILIKE '%buyer%'
+    THEN 'MOFU-RTBHOUSE'
+
+    WHEN utm_source = 'direct'
+        AND
+        utm_medium IS NULL
+    THEN 'direct'
+
+    WHEN utm_medium ILIKE 'sms'
+    THEN 'SMS'
+
+    WHEN utm_source = 'artofclick'
+    THEN 'TCA-ARTOFCLICK'
+
+    WHEN utm_source = 'Facebook Ads'
+    AND utm_campaign ILIKE '%-AI-%'
+    THEN 'TCA-FACEBOOK'
+
+    WHEN utm_source = 'Vungle_int'
+    THEN 'TCA-Vungle'
+
+    WHEN utm_source = 'website'
+        AND
+        utm_campaign = 'smartbanner'
+    THEN 'website-banner'
+
+    WHEN utm_source = 'googleadwords_int'
+    THEN 'TCA-google'
+
+    WHEN utm_source = 'googleadwordsoptimizer_int'
+    THEN 'TRT-google'
+
+    WHEN utm_source ILIKE '%FACEBOOK%'
+        AND
+        utm_campaign ILIKE 'FB-PE%'
+    THEN 'FB-PE'
+
+    WHEN utm_source = 'mobrain_int'
+    THEN 'mobrain_int'
+        
+    WHEN utm_source = 'mobomarket_int'
+    THEN 'mobomarket_int'
+
+    WHEN public_id IS NOT NULL
+    THEN 'order'
+
+    ELSE 'others'
